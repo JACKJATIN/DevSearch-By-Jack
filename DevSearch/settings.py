253d3fd9ebc6306a650f1ea554a2f7a2b3842b73
JACKJATIN <jatinjangid17@gmail.com>
2024-03-20 23:58:32 +0530

    aws storage 2

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -27,6 +27,11 @@ SECRET_KEY = 'django-insecure--4iws!uc#2j2cwb2llj!)k&lw3=+zy^2cxqrdj2h=nai8eiq*8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['demo-project-django.onrender.com','localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://demo-project-django.onrender.com',
+    
+]
 # Application definition
 
 INSTALLED_APPS = [
