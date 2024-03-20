3bf5f104bc032b0e2e54ef4ef355250d1f9875cc
JACKJATIN <jatinjangid17@gmail.com>
2024-03-20 21:13:58 +0530

    main commit 8

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -189,7 +189,8 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
