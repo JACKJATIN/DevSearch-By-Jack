21face30c13ce6d5f6cbd536ee5ab0dca0c94fb6
JACKJATIN <jatinjangid17@gmail.com>
2024-03-20 20:35:59 +0530

    main commit 4

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -191,7 +191,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
