9c78ccb620fe1e8a8c6cb181e0e108afaaab8379
JATIN JANGID <90373086+JACKJATIN@users.noreply.github.com>
2025-03-09 16:29:18 +0530

    Update bucket name aws settings.py

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -193,7 +193,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'devsearch-jackhijack'    
+# AWS_STORAGE_BUCKET_NAME = 'devsearch-jackhijack'
+AWS_STORAGE_BUCKET_NAME = 'djangodevsearchbucket'  
 
 
 if os.getcwd() == '/app':
